80002ff3ff4766d059d487217a750f3df068ca47
NEKOYASAN <hello@nekoyasan.me>
2024-12-21 00:21:23 +0900

    add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import adapter from '@hono/vite-dev-server/cloudflare'
 import honox from 'honox/vite'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [honox({ devServer: { adapter } }), build()]
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [honox({devServer: {adapter}}), build()],
+    build: {
+      rollupOptions: {
+        input: mode === "client" ? ["app/index.css"] : [],
+      }
+    }
+  }
 })
